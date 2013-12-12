ac89108f0b64ffd7940b2856c5ffb0ca9887fb75
Sean Girard <seangirard@yahoo.com>
2013-12-12 13:56:58 -0800

    archive

--- template-archive.php
+++ template-archive.php
@@ -12,6 +12,9 @@ Template Name: Archive (Posts)
 		<?php get_template_part('templates/content', 'page'); ?>
 
 		<div class="panel panel-default">
+			<div class="panel-heading">
+				Archive
+			</div>
 			<div class="panel-body">
 				<ul class="nav nav-pills nav-stacked">
 					<?php wp_get_archives(array('type'=>'postbypost','before'=>'<h3>','after'=>'</h3>')); ?>
