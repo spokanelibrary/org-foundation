2bafb6f31bed019bb3dff51b0c4f03fa95b95140
Sean Girard <seangirard@yahoo.com>
2013-12-12 14:08:57 -0800

    home page

--- template-home.php
+++ template-home.php
@@ -13,3 +13,52 @@ Template Name: - Home Page
 </p>
 
 <?php get_template_part('templates/content', 'page'); ?>
+
+
+<footer class="content-info container" role="contentinfo">
+  <div class="row">
+
+    <div class="col-sm-6">
+      <div class="panel panel-success panel-callout">
+        <p class="lead text-success">
+          About the Foundation
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Cras nec rutrum nisi. 
+          Vivamus eget suscipit purus, ut accumsan augue. Maecenas consequat mattis consequat.
+        </p>
+
+        <p>
+          <button class="btn btn-primary">
+            More &rarr;
+          </button>
+        </p>
+      </div>
+      
+    </div><!-- /.col -->
+    
+    <div class="col-sm-6">
+      <div class="panel panel-success panel-callout">
+        <p class="lead text-success">
+          Make a difference
+        </p>
+
+        <p>
+          Nunc eu consectetur odio, sit amet aliquet dolor. 
+          Vestibulum dictum eros neque, non posuere lacus iaculis at.
+          Etiam fringilla odio nec tempor molestie.
+        </p>
+
+        <p>
+          <button class="btn btn-primary">
+            More &rarr;
+          </button>
+        </p>
+      </div>
+      
+    </div><!-- /.col -->
+
+  </div><!-- /.row -->
+</footer>
--- templates/footer.php
+++ templates/footer.php
@@ -10,55 +10,6 @@
 </footer>
 
 
-<footer class="content-info container" role="contentinfo">
-	<div class="row">
-
-	  <div class="col-sm-6">
-	    <div class="panel panel-success panel-callout">
-	      <p class="lead text-success">
-	        About the Foundation
-	      </p>
-
-	      <p>
-	        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-	        Cras nec rutrum nisi. 
-	        Vivamus eget suscipit purus, ut accumsan augue. Maecenas consequat mattis consequat.
-	      </p>
-
-	      <p>
-	        <button class="btn btn-primary">
-	          More &rarr;
-	        </button>
-	      </p>
-	    </div>
-	    
-	  </div><!-- /.col -->
-	  
-	  <div class="col-sm-6">
-	    <div class="panel panel-success panel-callout">
-	      <p class="lead text-success">
-	        Make a difference
-	      </p>
-
-	      <p>
-	        Nunc eu consectetur odio, sit amet aliquet dolor. 
-	        Vestibulum dictum eros neque, non posuere lacus iaculis at.
-	        Etiam fringilla odio nec tempor molestie.
-	      </p>
-
-	      <p>
-	        <button class="btn btn-primary">
-	          More &rarr;
-	        </button>
-	      </p>
-	    </div>
-	    
-	  </div><!-- /.col -->
-
-	</div><!-- /.row -->
-</footer>
-
-
 <?php //include('org/footer.php'); ?>
 
 <!--[if (gte IE 6)&(lte IE 8)]>
