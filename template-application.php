2c05e254cfa31defdf99d5140445c5a0fd0a01bd
Sean Girard <seangirard@yahoo.com>
2013-12-12 15:29:37 -0800

    payment

--- template-application.php
+++ template-application.php
@@ -10,10 +10,11 @@ Template Name: Payment Info
 	<div class="col-sm-12">
 		<?php get_template_part('templates/page', 'header'); ?>
 		<?php get_template_part('templates/content', 'page'); ?>
+
+		<hr>
+
+		This is actually a payment form.
+
 	</div>
-
-	<hr>
-
-	This is actually a payment form.
 
 </div>
