f17cbbc10a720c861ee1e5b711bb3bdd83a40675
Sean Girard <seangirard@yahoo.com>
2013-12-12 12:38:21 -0800

    base font

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd6a3d6d1941b243cf81d1d74c4993862');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c07bff8137b83b66dc9c222d64b5bab4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
