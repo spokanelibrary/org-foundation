cf31ec95b0dd0dc4d119f5c898411369a743c04a
Sean Girard <seangirard@yahoo.com>
2013-12-12 14:06:18 -0800

    archive

--- template-archive.php
+++ template-archive.php
@@ -8,14 +8,14 @@ Template Name: Archive (Posts)
 <div class="row">
 
 	<div class="col-md-10 col-lg-9">
-		<?php get_template_part('templates/page', 'header'); ?>
+		<?php //get_template_part('templates/page', 'header'); ?>
 		<?php get_template_part('templates/content', 'page'); ?>
 
 		<?php
 			$args = array( 'numberposts' => '1' );
 			$recent_posts = wp_get_recent_posts( $args );
 			foreach( $recent_posts as $recent ){
-				echo '<h2>'.$recent["post_title"].'</h2>';
+				echo '<div class="page-header"><h1>'.$recent["post_title"].'</h1></div>';
 				echo $recent["post_content"];
 				//echo '<a href="' . get_permalink($recent['ID']) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a>';
 			}
