a1cd90361c05ed89eceef74e489ff1bf4c9d3c2a
Sean Girard <seangirard@yahoo.com>
2013-12-12 11:47:56 -0800

    template

--- template-home.php
+++ template-home.php
@@ -8,6 +8,8 @@ Template Name: - Home Page
 
 <?php //get_template_part('templates/page', 'header'); ?>
 
+<p>
 <img class="img-responsive" src="http://lorempixel.com/1200/300/people" >
+</p>
 
 <?php get_template_part('templates/content', 'page'); ?>
