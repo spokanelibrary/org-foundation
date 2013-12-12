a198142aedd35e73159e6b929b38584d006c5ab0
Sean Girard <seangirard@yahoo.com>
2013-12-12 14:42:55 -0800

    page

--- page.php
+++ page.php
@@ -1,2 +1,5 @@
+<p>
+<img class="img-responsive" src="http://lorempixel.com/1200/300/people" >
+</p>
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
