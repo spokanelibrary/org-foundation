675c1eeef91d4b7de0b9de7b9e0d57b8af5acb58
Sean Girard <seangirard@yahoo.com>
2013-12-13 14:03:13 -0800

    news

--- template-archive.php
+++ template-archive.php
@@ -28,7 +28,7 @@ Template Name: Archive (Posts)
 
 		<div class="panel panel-success" style="margin-top:20px;">
 			<div class="panel-heading">
-				<h4 class="serif"><?php echo get_the_title(); ?> <small>by <?php bloginfo('name'); ?></small></h4>
+				<h4 class="serif"><?php echo get_the_title(); ?> <small><em>by</em> <?php bloginfo('name'); ?></small></h4>
 			</div>
 			<div class="panel-body">
 				<ul class="nav nav-pills nav-stacked">
