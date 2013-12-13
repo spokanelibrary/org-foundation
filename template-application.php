c321bb9dfe5a99fc2d676d34cc01bbf75c1ab54f
Sean Girard <seangirard@yahoo.com>
2013-12-13 09:32:34 -0800

    donate

--- template-application.php
+++ template-application.php
@@ -21,17 +21,15 @@ Template Name: Payment Info
 
 	<div class="col-md-4">
 
-		<div style="text-muted">
-			<h3 style="text-muted">Lorem Ipsum</h3>
+			<h3>Lorem Ipsum</h3>
 			
 			<p>
-				The Spokane Public Library Foundation is a 501(c)(3) charitable organization registered with the IRS and Washington State.
+				<em>The Spokane Public Library Foundation is a 501(c)(3) charitable organization registered with the IRS and Washington State.</em>
 			</p>
 
 			<p>
 				All gifts are tax-deductible to the extent allowed by law.
 			</p>
-		</div>
 
 	</div>
 
