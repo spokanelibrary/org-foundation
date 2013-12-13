7fc79965dc9c640191043623dee4390941fe70ef
Sean Girard <seangirard@yahoo.com>
2013-12-13 09:04:32 -0800

    masthead

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -47,8 +47,8 @@
       </a>
     </div>
     <div class="pull-right" style="width: 320px; padding-top:20px;">
-      <em class="text-muted serif">"My best friend is a person who will give me a book I have not read."</em>
-      &ndash; Abraham Lincoln
+      <em class="text-muted serif">"My best friend is a person who will give me a book I have not read."
+      &ndash; Abraham Lincoln</em>
     </div>
   </div>
 </header><!-- /.navbar -->
