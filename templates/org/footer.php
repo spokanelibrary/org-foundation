9604ec2b6bc2c5ab342a65d62ce6a98497d7a18d
Sean Girard <seangirard@yahoo.com>
2013-12-13 12:36:51 -0800

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -29,7 +29,7 @@
           &nbsp;
         </p>
         <p>
-        <a href="#">Financial Information <small>FEIN 91-1917727</small></a>
+        <a href="#">Financials <small>(FEIN: 91-1917727)</small></a>
         </p>
         <p>
           <a href="mailto:mail@spokanelibraryfdtn.org">mail@spokanelibraryfdtn.org</a>
