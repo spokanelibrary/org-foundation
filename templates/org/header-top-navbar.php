8a8f53f465087dd859a2ab93c424145632cc3d24
Sean Girard <seangirard@yahoo.com>
2013-12-13 08:21:25 -0800

    masthead

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -48,11 +48,13 @@
     </div>
     <div class="pull-right" style="width: 320px; padding-top:20px;">
       <p>
-        <a class="btn btn-xs btn-link" href="http://www.spokanelibrary.org"><span class="text-success"><i class="glyphicon glyphicon-home"></i></span> Spokane Public Library Website</a>
+        <a class="btn btn-xs btn-link" href="http://www.spokanelibrary.org"><i class="glyphicon glyphicon-home"></i> Spokane Public Library Website</a>
       </p>
+      <!--
       <p>
-        <a class="btn btn-xs btn-link" href="http://www.facebook.com/spokanelibrary"><span class="text-success"><i class="glyphicon glyphicon-thumbs-up"></i></span> Spokane Public Library on Facebook</a>
+        <a class="btn btn-xs btn-link" href="http://www.facebook.com/spokanelibrary"><i class="glyphicon glyphicon-thumbs-up"></i> Spokane Public Library on Facebook</a>
       </p>
+      -->
     </div>
   </div>
 </header><!-- /.navbar -->
