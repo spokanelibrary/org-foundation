7f93fe2b75c490bcb5f2ff750444392b5ffd3562
Sean Girard <seangirard@yahoo.com>
2013-12-13 13:12:10 -0800

    donate

--- template-application.php
+++ template-application.php
@@ -53,7 +53,7 @@ Template Name: Payment Info
 				</div>
 
 				<div class="col-md-4">
-					<button type="submit" class="btn btn-default btn-success">Donate &rarr;</button>
+					<button type="submit" class="btn btn-default btn-success">Proceed &rarr;</button>
 				</div>
 
 			</div>
