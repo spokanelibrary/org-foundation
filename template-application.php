f1f813eb3a2d16857b5cc0b152644321df1c4d8f
Sean Girard <seangirard@yahoo.com>
2013-12-18 14:24:48 -0800

    donate

--- template-application.php
+++ template-application.php
@@ -198,6 +198,13 @@ Template Name: Payment Info
 						</span>
 
 						<p>
+							<button type="submit" class="btn btn-warning btn-block">
+								<i class="glyphicon glyphicon-credit-card"></i> 
+								Proceed to payment &rarr;
+							</button>
+						</p>
+
+						<p>
 							<label class="checkbox-inline">
 								<input type="checkbox" value="">
 								I prefer to give anonymously
@@ -220,50 +227,46 @@ Template Name: Payment Info
 							</div>
 						</div>
 
-						<p>
-							<button type="submit" class="btn btn-warning btn-block">
-								<i class="glyphicon glyphicon-credit-card"></i> 
-								Proceed to payment &rarr;
-							</button>
-						</p>
-
 						<label>
 							Consider sustaining support:
 						</label>	
-						<p>
-							<label class="radio-inline">
-							<input type="radio" name="" value="">
-							Make it <em>Monthly</em>
-							</label>
-							<br>
-							<label class="radio-inline">
-							<input type="radio" name="" value="">
-							Count me in <em>Quarterly</em>
-							</label>
-							<br>
-							<label class="radio-inline">
-							<input type="radio" name="" value="">
-							Ante up <em>Annually</em>
-							</label>
-						</p>
 
-						<p>
-							<label>
-								Repeat contributions&hellip;
-								<select class="form-control">
-									<option value="0" selected>Indefinately</option>
-									<option value="1">For 1 year</option>
-									<option value="2">For 2 years</option>
-									<option value="3">For 3 years</option>
-									<option value="4">For 4 years</option>
-									<option value="5">For 5 years</option>
-								</select>
-							</label>
-							<span class="help-block">
-								<strong>Note:</strong>
-								If you choose to make a recurring gift, you can cancel or modify the arrangement at any time.
-							</span>
-						</p>
+						<div class="collapse">
+							<p>
+								<label class="radio-inline">
+								<input type="radio" name="" value="">
+								Make it <em>Monthly</em>
+								</label>
+								<br>
+								<label class="radio-inline">
+								<input type="radio" name="" value="">
+								Count me in <em>Quarterly</em>
+								</label>
+								<br>
+								<label class="radio-inline">
+								<input type="radio" name="" value="">
+								Ante up <em>Annually</em>
+								</label>
+							</p>
+
+							<p>
+								<label>
+									Repeat contributions&hellip;
+									<select class="form-control">
+										<option value="0" selected>Indefinately</option>
+										<option value="1">For 1 year</option>
+										<option value="2">For 2 years</option>
+										<option value="3">For 3 years</option>
+										<option value="4">For 4 years</option>
+										<option value="5">For 5 years</option>
+									</select>
+								</label>
+								<span class="help-block">
+									<strong>Note:</strong>
+									If you choose to make a recurring gift, you can cancel or modify the arrangement at any time.
+								</span>
+							</p>
+						</div>
 
 				</div>
 
