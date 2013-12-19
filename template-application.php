854b0bb36031cb2177f81ac943b2b3c4fea4d6b7
Sean Girard <seangirard@yahoo.com>
2013-12-19 12:10:04 -0800

    donate

--- template-application.php
+++ template-application.php
@@ -137,7 +137,7 @@ Template Name: Payment Info
 							<div class="panel-heading">
 								<button type="button" class="btn btn-danger btn-block" data-toggle="collapse" data-target="#donate-sustain">
 									<small class="glyphicon glyphicon-remove"></small> 
-									<strong>No, thanks.</strong> 
+									<strong>Nevermind, thanks.</strong> 
 									<br>
 									<small>I'd rather make a one-time gift.</small>
 								</button>
