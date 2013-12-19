163a923381fb7319cbcc9e66c99b149da2b1cb15
Sean Girard <seangirard@yahoo.com>
2013-12-19 13:40:31 -0800

    donate

--- template-application.php
+++ template-application.php
@@ -45,12 +45,10 @@ Template Name: Payment Info
 	
 	
 
-	<!--
+	
 	<input type="hidden" name="cmd" value="_xclick-subscriptions">
 
 	<input type="hidden" name="a3" value="">
-	-->
-	
 	<input type="hidden" name="p3" value="1"> 
 	<input type="hidden" name="t3" value="M">
 	
