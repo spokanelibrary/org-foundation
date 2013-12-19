1e897096a6547ad45cfda8311ca3215cc6846839
Sean Girard <seangirard@yahoo.com>
2013-12-18 20:09:31 -0800

    donate

--- template-application.php
+++ template-application.php
@@ -114,8 +114,9 @@ Template Name: Payment Info
 						</p>
 						
 						<p>
+							<small class="glyphicon glyphicon-usd"></small>
 							<button type="button" class="btn btn-link" data-toggle="collapse" data-target="#donate-amount">
-								<small class="glyphicon glyphicon-usd"></small> 
+								 
 								Specify an amount&hellip;
 							</button>
 						</p>
