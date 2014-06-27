bd158c25ecc197f482fcf54e90f5d82f306c37c1
Sean Girard <seangirard@yahoo.com>
2014-06-27 09:32:37 -0700

    trivia

--- templates/org/forms/field/trivia-signup.php
+++ templates/org/forms/field/trivia-signup.php
@@ -2,17 +2,6 @@
 
   <div class="col-md-12">
     
-    <div class="panel panel-primary">
-      <div class="panel-body">
-        <p class="lead text-center" style="margin-bottom:0;">
-        <a href="/contact/">
-          <i class="glyphicon glyphicon-exclamation-sign"></i>
-          For general inquiries, please <strong>contact us</strong> &rarr;
-        </a>
-        </p>
-      </div><!-- /.panel-body -->
-    </div><!-- /.panel -->
-    
     <?php //echo do_shortcode('[spl_widget login-form label]'); ?>
     
     <div class="panel panel-default">
@@ -31,19 +20,19 @@
 
           <fieldset>
             <legend class="text-muted">
-              <small class="glyphicon glyphicon-question-sign"></small>
-              Request research assistance
+              <small class="glyphicon glyphicon-question-pencil"></small>
+              Register your team
             </legend>
-            <div class="form-group">
-              <label for="spl-form-question" class="col-sm-4 control-label">My research question</label>
-              <div class="col-sm-8">
-                <label class="text-muted form-control-static">Please be as specific as possible with your question.</label>
-                <textarea rows="6" class="form-control required" id="spl-form-question" name="spl-form[question]"></textarea>
-              </div>
-            </div>
             
             <?php include 'common/field-contact.php'; ?>
             <?php include 'common/field-submit.php'; ?>
+
+            <div class="form-group">
+              <label for="spl-form-question" class="col-sm-4 control-label">Comments or questions</label>
+              <div class="col-sm-8">
+                <textarea rows="6" class="form-control required" id="spl-form-question" name="spl-form[question]"></textarea>
+              </div>
+            </div>
 
           </fieldset>
         </form>
