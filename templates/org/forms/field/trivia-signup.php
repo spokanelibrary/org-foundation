f2c0c8c317314dc2cd7da4a392eb2a7484339cf5
Sean Girard <seangirard@yahoo.com>
2014-06-27 09:58:38 -0700

    trivia

--- templates/org/forms/field/trivia-signup.php
+++ templates/org/forms/field/trivia-signup.php
@@ -30,9 +30,9 @@
 
               <label class="col-sm-4 control-label">Team Members</span></label>
               <div class="col-sm-8">
-                <p>
+                <label class="control-label normal">
                   <b>Optional:</b> Fill out the names of your 3 team members.
-                </p>
+                </label>
                 <label for="spl-form-team-a" class="control-label normal">Team Member Name</label>
                 <input type="text" class="form-control" id="spl-form-team-a" name="spl-form[team][a]" placeholder="">
                 <label for="spl-form-team-b" class="control-label normal">Team Member Name</label>
