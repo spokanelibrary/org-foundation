627c703afbc9ab72bc04cea6d6fd40af9a71c218
Sean Girard <seangirard@yahoo.com>
2014-06-27 09:39:00 -0700

    trivia

--- templates/org/forms/field/common/shared/field-contact-info.php
+++ templates/org/forms/field/common/shared/field-contact-info.php
@@ -1,18 +1,25 @@
 <div class="form-group">
-  <label class="col-sm-4 control-label">My contact information</label>
+  
+  <label class="col-sm-4 control-label">Contact information</label>
   <div class="col-sm-8">
 
-		  	<label for="spl-form-email" class="control-label">Email address</label>
-		  	<input type="text" class="form-control" id="spl-form-email" name="spl-form[email]" placeholder="">
-		  	<label for="spl-form-phone" class="control-label">Phone number</label>
-		  	<input type="text" class="form-control" id="spl-form-phone" name="spl-form[phone]" placeholder="">
-
-		  	<label for="spl-form-street" class="control-label">Mailing Address</label>
-		  	<textarea rows="3" class="form-control" id="spl-form-street" name="spl-form[street]"></textarea>
-		  	<label for="spl-form-city-st" class="control-label">City, ST</label>
-		  	<input type="text" class="form-control" id="spl-form-city-st" name="spl-form[city-st]" placeholder="">
-		  	<label for="spl-form-zip" class="control-label">Zip code</label>
-		  	<input type="text" class="form-control" id="spl-form-zip" name="spl-form[zip]" placeholder="">
+  	<label for="spl-form-email" class="control-label">Email address</label>
+  	<input type="text" class="form-control" id="spl-form-email" name="spl-form[email]" placeholder="">
+  	<label for="spl-form-phone" class="control-label">Phone number</label>
+  	<input type="text" class="form-control" id="spl-form-phone" name="spl-form[phone]" placeholder="">
 
   </div>
+
+  <label class="col-sm-4 control-label">Mailing address</label>
+  <div class="col-sm-8">
+
+  	<label for="spl-form-street" class="control-label">Street (or P.O. Box)</label>
+  	<textarea rows="3" class="form-control" id="spl-form-street" name="spl-form[street]"></textarea>
+  	<label for="spl-form-city-st" class="control-label">City, ST</label>
+  	<input type="text" class="form-control" id="spl-form-city-st" name="spl-form[city-st]" placeholder="">
+  	<label for="spl-form-zip" class="control-label">Zip code</label>
+  	<input type="text" class="form-control" id="spl-form-zip" name="spl-form[zip]" placeholder="">
+
+  </div>
+
 </div>
