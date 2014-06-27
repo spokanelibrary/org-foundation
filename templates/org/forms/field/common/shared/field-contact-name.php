891baccc26d166d485a8c6f8208b19d961b106ca
Sean Girard <seangirard@yahoo.com>
2014-06-27 09:45:44 -0700

    trivia

--- templates/org/forms/field/common/shared/field-contact-name.php
+++ templates/org/forms/field/common/shared/field-contact-name.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-  <label for="spl-form-name" class="col-sm-4 control-label">Primary Contact (Name)</label>
+  <label for="spl-form-name" class="col-sm-4 control-label">Contact Person (Name)</label>
   <div class="col-sm-8">
     <input type="text" class="form-control required" id="spl-form-name" name="spl-form[name]" placeholder="">
   </div>
