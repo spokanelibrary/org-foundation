93a511f68bdd60624b1dc2b93227fe4142085b02
Sean Girard <seangirard@yahoo.com>
2014-06-27 09:24:12 -0700

    trivia

--- templates/org/forms/form-trivia-signup.php
+++ templates/org/forms/form-trivia-signup.php
@@ -2,7 +2,7 @@
 $crass = wp_get_crass_response();
 print_r($crass);
 if ( !empty($crass->request) ) {
-  include 'field/triva-signup-response.php';
+  include 'field/trivia-signup-response.php';
   } else {
   include 'field/trivia-signup.php';
 }
